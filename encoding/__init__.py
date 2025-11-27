a00e634fc90a6629601fa0635ba43a22b1ece40d
Shema Leandre <iamshemaleandre@gmail.com>
2025-11-27 07:21:34 +0200

    refactor: update package __init__ files with proper exports

--- encoding/__init__.py
+++ encoding/__init__.py
@@ -5,6 +5,19 @@ Encoding Techniques Package
 from FrequencyEncoding import frequency_encode
 from labelEncoding import label_encode
 from one_hot_encoding import one_hot_encode
+from encoding.binary_encoding import binary_encode
+from encoding.target_encoding import target_encode
+from encoding.hash_encoding import hash_encode
+from encoding.ordinal_encoding import ordinal_encode
+from encoding.mean_encoding import mean_encode
 
-__all__ = ['frequency_encode', 'label_encode', 'one_hot_encode']
-
+__all__ = [
+    'frequency_encode',
+    'label_encode',
+    'one_hot_encode',
+    'binary_encode',
+    'target_encode',
+    'hash_encode',
+    'ordinal_encode',
+    'mean_encode'
+]
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,16 @@
 # Utility functions package
 
+from utils.data_loader import load_csv, load_excel
+from utils.validation import check_missing_values, check_duplicates
+from utils.preprocessing import handle_missing_values
+from utils.feature_scaling import standard_scale, minmax_scale
+
+__all__ = [
+    'load_csv',
+    'load_excel',
+    'check_missing_values',
+    'check_duplicates',
+    'handle_missing_values',
+    'standard_scale',
+    'minmax_scale'
+]
