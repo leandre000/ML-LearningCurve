c33ca6d9a3be205895bf17b9955814d913ac7445
Shema Leandre <iamshemaleandre@gmail.com>
2025-11-27 08:18:40 +0200

    refactor: update encoding package with version and all encoding techniques

--- encoding/__init__.py
+++ encoding/__init__.py
@@ -1,5 +1,7 @@
 """
 Encoding Techniques Package
+
+A comprehensive collection of categorical encoding techniques for machine learning.
 """
 
 from FrequencyEncoding import frequency_encode
@@ -10,6 +12,12 @@ from encoding.target_encoding import target_encode
 from encoding.hash_encoding import hash_encode
 from encoding.ordinal_encoding import ordinal_encode
 from encoding.mean_encoding import mean_encode
+from encoding.woe_encoding import woe_encode
+from encoding.leave_one_out_encoding import leave_one_out_encode
+from encoding.backward_difference_encoding import backward_difference_encode
+from encoding.helmert_encoding import helmert_encode
+
+__version__ = "1.0.0"
 
 __all__ = [
     'frequency_encode',
@@ -19,5 +27,9 @@ __all__ = [
     'target_encode',
     'hash_encode',
     'ordinal_encode',
-    'mean_encode'
+    'mean_encode',
+    'woe_encode',
+    'leave_one_out_encode',
+    'backward_difference_encode',
+    'helmert_encode'
 ]
