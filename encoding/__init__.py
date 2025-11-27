f13c620136acdbaea27e0577657be275f8640dda
Shema Leandre <iamshemaleandre@gmail.com>
2025-11-27 06:11:23 +0200

    refactor: create encoding package with unified imports

---
+++ encoding/__init__.py
@@ -0,0 +1,10 @@
+"""
+Encoding Techniques Package
+"""
+
+from FrequencyEncoding import frequency_encode
+from labelEncoding import label_encode
+from one_hot_encoding import one_hot_encode
+
+__all__ = ['frequency_encode', 'label_encode', 'one_hot_encode']
+
